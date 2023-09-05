200923db1ea99cd58e3d0b2eed9aa0df2dc5e705
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-09-05 10:19:17 +0530

    har din sappy

---
+++ day10/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+for (int i = 0; i < v.size(); i++)
+    // {
+    //     for (int j = i + 1; j < v.size(); j++)
+    //     {
+    //         if (v[j] % v[i] == 0)
+    //         {
+    //             v.erase(v.begin() + j);
+    //         }
+    //     }
+    // }
